502893d4027e2b38f57ec12c395d2441a86459b8
txh <txh@qq.com>
2023-05-09 22:01:19 +0800

    1

--- download_pdf/download_pdf.py
+++ download_pdf/download_pdf.py
@@ -1,12 +1,77 @@
 import datetime
 import json
+import logging
 import os.path
 import shutil
 import time
-
+import logging.handlers
 from selenium import webdriver
+def create_folder(folder):
+    folder = os.path.abspath(folder)
+    if not os.path.exists(folder):
+        try:
+            os.makedirs(folder)
+            msg = 'Success create folder:{}'.format(folder)
+            print(msg)
+        except Exception as e:
+            msg = 'Failed create folder:{}, exception:{}'.format(folder, e)
+            print(msg)
 
+def init_logger(log_name='logs/log.log',
+                logger_string='ROOT',
+                max_bytes=1024 * 1024 * 1,
+                backup_count=10,
+                mode='a',
+                fmt_string='[%(asctime)s][%(name)s][%(levelname)s]%(message)s',
+                record_level=logging.INFO,
+                console_level=logging.INFO,
+                file_level=logging.INFO):
+    """
+    CRITICAL = 50
+    FATAL = CRITICAL
+    ERROR = 40
+    WARNING = 30
+    WARN = WARNING
+    INFO = 20
+    DEBUG = 10
+    NOTSET = 0
+    :param fmt_string:
+    :param logger_string:
+    :param mode:
+    :param log_name:
+    :param max_bytes:
+    :param backup_count:
+    :param record_level:
+    :param console_level:
+    :param file_level:
+    :return:
+    """
+    create_folder(os.path.dirname(log_name))
+    fmt = logging.Formatter(fmt_string)  # 设置日志格式
+    logger = logging.getLogger(logger_string)
+    logger.setLevel(record_level)
 
+    stream_handler = logging.StreamHandler()  # 控制台日志句柄,设置则可以打印到控制台
+    stream_handler.setLevel(console_level)  # 设置打印到控制台日志等级
+    stream_handler.setFormatter(fmt)
+    if stream_handler not in logger.handlers:
+        logger.addHandler(stream_handler)  # 添加控制台句柄
+    # 设置回滚日志句柄
+    rollback_handler = logging.handlers.RotatingFileHandler(log_name, mode, max_bytes, backup_count)
+
+    rollback_handler.setLevel(file_level)  # 设置回滚日志记录INFO以及以上信息
+    rollback_handler.setFormatter(fmt)
+    if rollback_handler not in logger.handlers:
+        logger.addHandler(rollback_handler)  # 添加回滚日志句柄
+    return logger  # 返回句柄，以便于使用
+
+today = datetime.datetime.now().strftime("%Y-%m-%d")
+print(f"today is {today}")
+log_name = f"{today}.log"
+if os.path.isfile(log_name):
+    os.remove(log_name)
+    print(f"删除已经存在的日志:{log_name}")
+log = init_logger(log_name)
 def download_pdf():
     path = r"D:\下载"
     url = 'http://edc.micron.com/mti/SEG001/APPENG/B58R_NAND_Design_Datasheet.pdf'
@@ -15,13 +80,13 @@ def download_pdf():
     path = os.path.abspath(path)
     if not os.path.isdir(path):
         os.makedirs(path)
-        # print(f"create dir:{path}")
+        # log.info(f"create dir:{path}")
 
     name = url.split("/")[-1]
     filename = os.path.join(path, name)
     if os.path.isfile(filename):
         os.remove(filename)
-        # print(f"delete file:{filename}")
+        # log.info(f"delete file:{filename}")
 
     options = webdriver.ChromeOptions()
     options.add_experimental_option("excludeSwitches", ["enable-logging"])
@@ -35,18 +100,18 @@ def download_pdf():
     web.get(url)
     time.sleep(5)  # TODO 等待下载的时间
 
-    # print(f"save to:{filename}")
+    log.info(f"save to:{filename}")
     return filename
 
 
 def is_update(old: list, save_name: str, size: int):
     now = datetime.datetime.now().strftime("%Y_%m_%d")
-    print(f"today is {now}")
+    log.info(f"today is {now}")
     for one in old:
         if one['size'] == size:
-            print("not update~~~~")
+            log.info("not update~~~~")
             return
-    print(f"updated!!!, newest {save_name}")
+    log.info(f"updated!!!, newest {save_name}")
 
 
 def create_folder(folder):
@@ -55,10 +120,10 @@ def create_folder(folder):
         try:
             os.makedirs(folder)
             msg = 'Success create folder:{}'.format(folder)
-            # print(msg)
+            # log.info(msg)
         except Exception as e:
             msg = 'Failed create folder:{}, exception:{}'.format(folder, e)
-            print(msg)
+            log.info(msg)
 
 
 def copy_file(src, dst):
@@ -68,10 +133,10 @@ def copy_file(src, dst):
     try:
         shutil.copy2(src, dst)
         msg = 'Success copy file:{} to :{}'.format(os.path.abspath(src), os.path.abspath(dst))
-        # print(msg)
+        # log.info(msg)
     except Exception as e:
         msg = 'Fail copy file:{} to :{}, exception:{}'.format(os.path.abspath(src), os.path.abspath(dst), e)
-        print(msg)
+        log.info(msg)
 
 
 def delete_file(file):
@@ -80,25 +145,26 @@ def delete_file(file):
         try:
             os.remove(file)
             msg = 'Success delete file:{}'.format(file)
-            # print(msg)
+            # log.info(msg)
         except Exception as e:
             msg = 'Failed delete file:{}, exception:{}'.format(file, e)
-            print(msg)
+            log.info(msg)
 
 
 def compare(filename: str):
     now = datetime.datetime.now().strftime("%Y_%m_%d")
     version_file = "ExistingVersion.txt"
     info = {'time': now, 'filename': filename, 'size': os.path.getsize(filename)}
+    log.info(f"info:{info}")
     if not os.path.isfile(version_file):
         with open(version_file, 'w') as f:
             json.dump([info], f, indent=4)
-        # print(f'first save info to {version_file}')
+        log.info(f'first save info to {version_file}')
     else:
-        # print(f'open exist file:{version_file}')
+        log.info(f'open exist file:{version_file}')
         with open(version_file, 'r') as f:
             old: list = json.load(f)
-            # print(f"old info:{old}")
+            # log.info(f"old info:{old}")
         is_update(old, filename, os.path.getsize(filename))
         new = old.copy()
         new.append(info)
@@ -107,7 +173,7 @@ def compare(filename: str):
     # dir_name = os.path.dirname(filename)
     name, suffix = os.path.splitext(filename)
     new_name = name + '_' + now + suffix
-    # print(f"new name:{new_name}")
+    # log.info(f"new name:{new_name}")
     copy_file(filename, new_name)
     delete_file(filename)
 
